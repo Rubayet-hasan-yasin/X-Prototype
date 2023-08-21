3bd9ece8dbb4ea6073ceb66003eca7189a0e2513
rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2023-08-21 19:15:28 +0600

    toggle button update

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,5 @@
 import File_input from "./File_input";
+import ToggleButton from "./ToggleButton";
 
 const Form = () => {
     return (
@@ -39,11 +40,7 @@ const Form = () => {
 
                             {/* Manually insert data  */}
                             {/* Toggle Switch */}
-                            <label className="relative inline-flex items-center cursor-pointer">
-                                <input type="checkbox" value="" className="sr-only peer" />
-                                    <div className="w-11 h-6 bg-gray-400 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
-                                    <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300 select-none">Manually insert</span>
-                            </label>
+                            <ToggleButton/>
                             
 
 
